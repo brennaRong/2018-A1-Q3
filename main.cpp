a3d32f7d53a843b39bc234ee73d1fe0585c4d94d
brennaRong <35971894+brennaRong@users.noreply.github.com>
2018-02-08 20:36:37 +0200

    Q3

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// 1. Read data from input file
+// 2. Update balance to screen after each transaction
+// 3. When balance drops below R1000 a R25 service fee is charged at the end of the month
+// 4. Deposits to be marked with Ct at end
+
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
